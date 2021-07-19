f99ecece2cac36419803b465b8dc2d0112e1d149
jjy423 <jjy423@likelion.org>
2021-07-19 10:17:59 +0900

    second commit

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -15,12 +15,13 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+from . import my_settings
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o5#9)p%95g+5vcoc(tq==v2l%t$o-t%(j1##ow5jfx4u#u^@+*'
+SECRET_KEY = my_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
